d3379d69dae2e17cf50644ee8e8d842bda91ebef
Gangavaram Sreevibha <123618407+sgangavaram@users.noreply.github.com>
2024-06-14 22:33:36 +0530

    Merging frontend and backend

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
 import pickle
-from nltk.stem import PorterStemmer
-from nltk.stem import WordNetLemmatizer
-from nltk.corpus import stopwords
 import re
 import nltk
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk.corpus import stopwords
 from flask import Flask, request, render_template
 
 nltk.download('stopwords')
@@ -32,14 +31,12 @@ def lemmatizing(text):
     return stemSentence
 
 def stemming(text):
-
     stemmer = PorterStemmer()
     stemmed_sentence = ""
     for word in text.split():
         stem = stemmer.stem(word)
         stemmed_sentence += stem
         stemmed_sentence += " "
-
     stemmed_sentence = stemmed_sentence.strip()
     return stemmed_sentence
 
@@ -52,28 +49,23 @@ def test(text, model, tfidf_vectorizer):
     predicted = model.predict(text_vector)
     newmapper = {0: 'Fantasy', 1: 'Science Fiction', 2: 'Crime Fiction',
                  3: 'Historical novel', 4: 'Horror', 5: 'Thriller'}
-
     return newmapper[predicted[0]]
 
-file = open('bookgenremodel.pkl', 'rb')
-model = pickle.load(file)
-file.close()
-file1 = open('tfdifvector.pkl', 'rb')
-tfidf_vectorizer = pickle.load(file1)
-file1.close()
+with open('bookgenremodel.pkl', 'rb') as file:
+    model = pickle.load(file)
+
+with open('tfdifvector.pkl', 'rb') as file:
+    tfidf_vectorizer = pickle.load(file)
+
 app = Flask(__name__)
+
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-
     if request.method == 'POST':
-
-        mydict = request.form
-        text = mydict["summary"]
+        text = request.form['summary']
         prediction = test(text, model, tfidf_vectorizer)
-
         return render_template('index.html', genre=prediction, text=str(text)[:100], showresult=True)
     return render_template('index.html')
 
-
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=8082)
+    app.run(host='0.0.0.0', port=8082)
